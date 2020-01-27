ace8b71b1a01e7fa7431af573d5c813b9ea8e76e
luis fernando cifuentes <luis.fernando.cifuentes@correounivalle.edu.co>
2020-01-27 10:12:15 -0500

    add products seeder

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'id' => 1,
+            'name' => 'Cuenta De Ahorros'
+        ]);
+    }
+}
